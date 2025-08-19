1428b0144238c5d943b08444c026d3ec63d0e043
bogas08 <lopezgabriel0802>
2025-08-18 23:11:27 -0400

    iniciando repo 121

---
+++ practica1/src/main/java/ejer1/main.java
@@ -0,0 +1,15 @@
+
+package ejer1;
+
+public class main {
+   
+    public static void main(String[] args) {
+        ecuacion e1=new ecuacion(9,4,3,-5,-6,-21);
+        System.out.println(e1.tieneSolucion());
+        System.out.println(e1.getX());
+         System.out.println(e1.getY());
+        //int e=e1.getX();
+
+    }
+    
+}

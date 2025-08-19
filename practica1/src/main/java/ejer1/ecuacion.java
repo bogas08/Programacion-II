b6d91417ba41cd6baf4e634f01c32f1b4deb71ce
bogas08 <147743579+bogas08@users.noreply.github.com>
2025-08-18 23:31:07 -0400

    carpetas
    inicial

---
+++ practica1/src/main/java/ejer1/ecuacion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejer1;
+
+public class ecuacion {
+    private int x;
+    private int y;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+
+    public ecuacion(int a, int b, int c, int d,int e, int f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+    public boolean tieneSolucion(){
+        if(a*d-b*c != 0){
+            return true; }
+        else{
+        return false;  }
+        
+    }
+    public int getX(){
+        int n=e*d-b*f;
+        int de=a*d-b*c;
+        x=n/de;
+        return x;
+    }
+
+    public int getY() {
+        int nom=a*f-e*c;
+        int den=a*d-b*c;
+        y=nom/den;
+        return y;
+    }
+    
+    
+}
